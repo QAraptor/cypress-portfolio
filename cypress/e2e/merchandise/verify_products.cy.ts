5804b1a1f90d5ab96c863368b35d2f88722a61a2
QAraptor <davidmmacfarlane@gmail.com>
2025-12-26 11:06:21 -0500

    added products testcase

--- cypress/e2e/merchandise/verify_products.cy.ts
+++ cypress/e2e/merchandise/verify_products.cy.ts
@@ -5,15 +5,34 @@
 describe('Login', () => {
   beforeEach(() => {
     cy.visit('https://www.automationexercise.com');
-  })
-  
-   it('Test Case 8: Verify All Products and product detail page', () => {
+  });
+
+  it('Test Case 8: Verify All Products and product detail page', () => {
     cp.homePage.verifyHomeTitle();
     cp.homePage.goToProductPage();
     cp.productPage.verifyPageTitle();
     cp.productPage.verifyProductList();
-    
-   
+    cp.productPage.clickFirstProduct();
+    cp.productPage.verifyDefaultFilters();
   });
-  
-})
+
+
+  it('Test Case 12: Add Products in Cart', () =>{
+
+
+    
+  }
+
+  /*
+1. Launch browser
+2. Navigate to url 'http://automationexercise.com'
+3. Verify that home page is visible successfully
+4. Click 'Products' button
+5. Hover over first product and click 'Add to cart'
+6. Click 'Continue Shopping' button
+7. Hover over second product and click 'Add to cart'
+8. Click 'View Cart' button
+9. Verify both products are added to Cart
+10. Verify their prices, quantity and total price
+  */
+});
--- cypress/pages/Products.ts
+++ cypress/pages/Products.ts
@@ -1,32 +1,49 @@
 /// <reference types="cypress" />
-
 interface ProductsPageElements {
   titleTextHeader: () => Cypress.Chainable<JQuery<HTMLElement>>;
   productList: () => Cypress.Chainable<JQuery<HTMLElement>>;
-  pageTitle: () => string;
+  viewButton: () => Cypress.Chainable<JQuery<HTMLElement>>;
 }
 
 export class ProductsPage {
-  elements = {
-    titleTextHeader: () => cy.get(".title.text-center"),
-    productList: () => cy.get(".product-image-wrapper"),
-    pageTitle: () => "All Products"
+  readonly pageTitle = 'All Products';
+  private readonly filterLabels = ['Category', 'Brand', 'Condition', 'Availability', 'Rs'] as const;
 
-
-  }; 
+  elements: ProductsPageElements = {
+    titleTextHeader: () => cy.get('.title-text-center'),
+    productList: () => cy.get('.product-image-wrapper'),
+    viewButton: () => cy.get("a[href='/product_details/1']"),
+  };
 
   verifyPageTitle() {
-    this.elements.titleTextHeader()
-      .should('be.visible')
-      .and('contain.text', this.elements.pageTitle());
+    cy.contains(this.pageTitle).should('be.visible');
   }
- verifyProductList() {
-  this.elements.productList()
-    .should('have.length.greaterThan', 0)
-    .each(($el) => {
-      cy.wrap($el).should('be.visible');
+
+  verifyProductList() {
+    this.elements
+      .productList()
+      .should('have.length.greaterThan', 0)
+      .each(($el) => {
+        cy.wrap($el).should('be.visible');
+      });
+  }
+
+  verifyDefaultFilters(): void {
+    this.filterLabels.forEach((label) => {
+      cy.contains(label).should('be.visible');
     });
-}
+  }
+
+  clickViewButton() {
+    this.elements.viewButton().click();
+  }
+  viewProductLinks() {
+    return cy.get('a[href^="/product_details"]');
+  }
+
+  clickFirstProduct() {
+    this.viewProductLinks().first().click();
+  }
 }
 
-  export const productPage = new ProductsPage();
+export const productPage = new ProductsPage();
